1fc143b984b82fa3ad7476e35fd8912d2abcb1a5
soarGuo <soarguo@163.com>
2020-12-17 20:49:17 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,27 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: 2,
+      c: [],
+      d: {
+        a: 1,
+        b: '2'
+      }
+    }
+  },
+  methods: {
+    test () {
+      // this.a.abc()
+    }
+  }
+})
+</script>
+
 <style lang="scss" scoped>
 </style>
